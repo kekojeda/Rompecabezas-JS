4b943635b9ea4f76a8062025914f428dc54c12ac
keko <kekojeda@gmail.com>
2019-09-03 19:44:04 -0300

    Version 1.0

---
+++ ejercicioAcamica.js
@@ -0,0 +1,80 @@
+
+
+// definir las variables que sean necesarias.
+
+
+
+1 - // Hacer una funcion, que ingresando tu edad, calcule el año en que naciste.
+  // new Date().getFullYear() // retorna el año actual.
+
+
+
+
+2 - // Hacer una funcion que recibe un año y determina si es bisiesto, retorna un booleano (año bisiesto cada 4 años).
+
+
+
+
+3 - // Hacer una funcion que reciba un id de banco (numerico) y retorne el nombre
+// 1: "Macro";
+// 2: "Patagonia";
+// 3: "Santander";
+// 4: "Nacion";
+
+
+4 - // hacer una funcion que reciba un id de tarjeta (numerico) y retorne el nombre de la misma
+// 1: "Naranja";
+// 2: "Visa";
+// 3: "Master";
+// 4: "Cordobesa";
+
+
+
+5 - // hacer una funcion que reciba dia, mes, año y retorne el nombre del dia. ejemplo (20/08/2019 => martes) 
+// usar la funcion     new Date(dia, mes, anio).getDay(); // esto retorna el numero de dia de la semana
+// 0 - domingo
+// 1 - lunes
+// 2 - martes.
+
+
+
+
+// pedir por pantalla 
+// nombre completo
+// edad actual (numerico).
+// dia de nacimiento, numerico.
+// mes de nacimiento, numerico.
+
+// calcular el año de nacimiento con la funcion del punto 1.
+
+// obtener el dia de nacimiento con la funcion del punto 5.
+
+// consultar si el usuario quiere crear una cuenta.
+// si quiere consultarle con que entidad 
+// prompt("Ingrese el banco donde creara la cuenta: \n 1- Macro \n 2- Patagonia \n 3- Santander \n 4- Nacion");
+// usar la funcion del punto 3 para obtener el nombre de la entidad.
+
+// si no quiere la cuenta. otorgarle una tarjeta de credito.
+// prompt("Se le otorgara solo una tarjeta de credito: \n 1- Naranja \n 2- Visa \n 3- Master \n 4- Cordobesa");
+// usar la funcion del punto 4 para determinar la tarjeta seleccionada.
+
+
+// usar la funcion del punto 2 para determinar si el año de nacimiento es bisiesto. 
+// de ser bisiesto 
+// acreditar 500 en la cuenta del usuario (tarjeta o cuenta, lo que haya seleccionado.)
+// si no es bisiesto
+// acreditar solo 200.
+
+
+// mostrar la siguiente informacion por pantalla.
+/**
+ * mostrar 
+ * Nombre Completo: 
+ * Fecha de nacimiento: 
+ * Dia de nacimiento: 
+ * Año bisiesto: Si / No
+ * Tipo Cuenta:  Bancaria/ Tarjeta
+ * Nombre Tarjeta / Nombre Banco
+ * Saldo de cuenta: $
+ */
+
--- js/juego.js
+++ js/juego.js
@@ -1,14 +1,14 @@
 // Arreglo que contiene las intrucciones del juego 
-var instrucciones = [];
+var instrucciones = ["Instruccion 1", "Instruccion 2", "Instruccion 3", "Instruccion 4", "Instruccion 5", "Instruccion 6"];
 // Arreglo para ir guardando los movimientos que se vayan realizando
 var movimientos = [];
 
 // Representación de la grilla. Cada número representa a una pieza.
 // El 9 es la posición vacía
 var grilla = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
 ];
 
 /* Estas dos variables son para guardar la posición de la pieza vacía. 
@@ -21,21 +21,51 @@ Cada elemento de este arreglo deberá ser mostrado en la lista con id 'lista-ins
 Para eso deberás usar la función ya implementada mostrarInstruccionEnLista().
 Podés ver su implementación en la ultima parte de este codigo. */
 function mostrarInstrucciones(instrucciones) {
-    //COMPLETAR
+  for (i = 0; i < instrucciones.length; i++) {
+    mostrarInstruccionEnLista(instrucciones[i], 'lista-instrucciones');
 }
+}
+
+
+
+/*HACEEER
+Mostrar el último movimiento podría haber sido implementado sólo con una variable, pero queremos recordar todos los movimientos realizados para que te animes a implementar otras funcionalidades por tu cuenta. Por ejemplo: ¿cómo sería mostrar los últimos 5 movimientos? ¿O mostrar al final del juego todos los movimientos realizados?
+
+/*
 
 /* COMPLETAR: Crear función que agregue la última dirección al arreglo de movimientos
 y utilice actualizarUltimoMovimiento para mostrarlo en pantalla */
 
+function agregarMovimiento(direccion) {
+  movimientos.push(direccion);
+  actualizarUltimoMovimiento(direccion);
+
+}
+
 /* Esta función va a chequear si el Rompecabezas esta en la posicion ganadora. 
 Existen diferentes formas de hacer este chequeo a partir de la grilla. */
 function chequearSiGano() {
-    //COMPLETAR
+  var control = 1;
+  for (i = 0; i < grilla.length; i++) {
+    for (j = 0; j < grilla.length; j++) {
+      if (grilla[i][j] == control) {
+        control++;
+      }
+    }
+  }
+  if (control == 10) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 // Implementar alguna forma de mostrar un cartel que avise que ganaste el juego
 function mostrarCartelGanador() {
-    //COMPLETAR
+  if(chequearSiGano()){
+    alert("GANASTEEEEEEEEEEEEEEEEEEEEEEE!")
+  }
 }
 
 /* Función que intercambia dos posiciones en la grilla.
@@ -49,18 +79,32 @@ En vez de intercambiar esos valores vamos a terminar teniendo en ambas posicione
 Se te ocurre cómo solucionar esto con una variable temporal?
 */
 function intercambiarPosicionesGrilla(filaPos1, columnaPos1, filaPos2, columnaPos2) {
-    //COMPLETAR
+  //COMPLETAR
+  
+  var temp1 =  grilla[filaPos1][columnaPos1];
+  var temp2=  grilla[filaPos2][columnaPos2];
+
+  grilla[filaPos1][columnaPos1] = temp2;
+  grilla[filaPos2][columnaPos2] = temp1;
+
+
 }
 
 // Actualiza la posición de la pieza vacía
 function actualizarPosicionVacia(nuevaFila, nuevaColumna) {
-    //COMPLETAR
+  //COMPLETAR
+  filaVacia = nuevaFila;
+  columnaVacia = nuevaColumna;
 }
 
 
 // Para chequear si la posicón está dentro de la grilla.
 function posicionValida(fila, columna) {
-    //COMPLETAR
+  if(fila>=0 && fila <=2 && columna >=0 && columna <=2){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 /* Movimiento de fichas, en este caso la que se mueve es la blanca intercambiando su posición con otro elemento.
@@ -74,34 +118,37 @@ function moverEnDireccion(direccion) {
     nuevaFilaPiezaVacia = filaVacia - 1;
     nuevaColumnaPiezaVacia = columnaVacia;
   }
-    
+
   // Mueve pieza hacia arriba, reemplazandola con la blanca
   else if (direccion === codigosDireccion.ARRIBA) {
     nuevaFilaPiezaVacia = filaVacia + 1;
     nuevaColumnaPiezaVacia = columnaVacia;
   }
-    
+
   // Mueve pieza hacia la derecha, reemplazandola con la blanca
   else if (direccion === codigosDireccion.DERECHA) {
-    //COMPLETAR
+    nuevaColumnaPiezaVacia = columnaVacia + 1;
+    nuevaFilaPiezaVacia = filaVacia;
   }
-    
+
   // Mueve pieza hacia la izquierda, reemplazandola con la blanca
   else if (direccion === codigosDireccion.IZQUIERDA) {
-    // COMPLETAR
+    nuevaColumnaPiezaVacia = columnaVacia - 1;
+    nuevaFilaPiezaVacia = filaVacia;
   }
 
   /* A continuación se chequea si la nueva posición es válida, si lo es, se intercambia. 
   Para que esta parte del código funcione correctamente deberás haber implementado 
   las funciones posicionValida, intercambiarPosicionesGrilla y actualizarPosicionVacia */
 
-    if (posicionValida(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia)) {
-        intercambiarPosiciones(filaVacia, columnaVacia, nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
-        actualizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+  if (posicionValida(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia)) {
+    intercambiarPosiciones(filaVacia, columnaVacia, nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+    actualizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
 
-  //COMPLETAR: Agregar la dirección del movimiento al arreglo de movimientos
+    //COMPLETAR: Agregar la dirección del movimiento al arreglo de movimientos
+    agregarMovimiento(direccion);
 
-    }
+  }
 }
 
 
@@ -121,10 +168,10 @@ el uso de números confusos en tu código. Para referirte a la dir
 izquierda, en vez de usar el número 37, ahora podés usar:
 codigosDireccion.IZQUIERDA. Esto facilita mucho la lectura del código. */
 var codigosDireccion = {
-    IZQUIERDA: 37,
-    ARRIBA: 38,
-    DERECHA: 39,
-    ABAJO: 40
+  IZQUIERDA: 37,
+  ARRIBA: 38,
+  DERECHA: 39,
+  ABAJO: 40
 }
 
 /* Funcion que realiza el intercambio logico (en la grilla) y ademas actualiza
@@ -195,17 +242,17 @@ function mezclarPiezas(veces) {
   if (veces <= 0) {
     return;
   }
-  
+
   var direcciones = [codigosDireccion.ABAJO, codigosDireccion.ARRIBA,
-      codigosDireccion.DERECHA, codigosDireccion.IZQUIERDA
-    ];
+  codigosDireccion.DERECHA, codigosDireccion.IZQUIERDA
+  ];
 
   var direccion = direcciones[Math.floor(Math.random() * direcciones.length)];
   moverEnDireccion(direccion);
 
-  setTimeout(function() {
-      mezclarPiezas(veces - 1);
-    }, 100);
+  setTimeout(function () {
+    mezclarPiezas(veces - 1);
+  }, 100);
 }
 
 /* capturarTeclas: Esta función captura las teclas presionadas por el usuario. Javascript
@@ -214,7 +261,7 @@ base a eso hacer algo. No es necesario que entiendas como funciona esto ahora,
 en el futuro ya lo vas a aprender. Por ahora, sólo hay que entender que cuando
 se toca una tecla se hace algo en respuesta, en este caso, un movimiento */
 function capturarTeclas() {
-  document.body.onkeydown = (function(evento) {
+  document.body.onkeydown = (function (evento) {
     if (evento.which === codigosDireccion.ABAJO ||
       evento.which === codigosDireccion.ARRIBA ||
       evento.which === codigosDireccion.DERECHA ||
@@ -222,24 +269,24 @@ function capturarTeclas() {
 
       moverEnDireccion(evento.which);
 
-        var gano = chequearSiGano();
-        if (gano) {
-          setTimeout(function() {
-              mostrarCartelGanador();
-              }, 500);
-            }
-            evento.preventDefault();
-        }
-    })
+      var gano = chequearSiGano();
+      if (gano) {
+        setTimeout(function () {
+          mostrarCartelGanador();
+        }, 500);
+      }
+      evento.preventDefault();
+    }
+  })
 }
 
 /* Se inicia el rompecabezas mezclando las piezas 60 veces 
 y ejecutando la función para que se capturen las teclas que 
 presiona el usuario */
 function iniciar() {
-    mostrarInstrucciones(instrucciones);
-    mezclarPiezas(30);
-    capturarTeclas();
+  mostrarInstrucciones(instrucciones);
+  mezclarPiezas(30);
+  capturarTeclas();
 }
 
 // Ejecutamos la función iniciar
